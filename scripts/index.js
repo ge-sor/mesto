50323ab384ca39170dcfcc81d09c3554604b9fed
German Sorokin <ger.sorokin@gmail.com>
2020-12-27 22:13:22 +0300

    main

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
 let openButton = document.querySelector('.button__edit-info');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.button__popup_close');
 
-console.log(openButton);
+
+openButton.addEventListener('click', () => {
+  popup.classList.add('popup_opened')
+})
+
+closeButton.addEventListener('click', () => {
+  popup.classList.remove('popup_opened')
+})
+
+popup.addEventListener('click', (event) => {
+  if (event.target === popup) {
+    popup.classList.remove('popup_opened')
+  }
+
+})
