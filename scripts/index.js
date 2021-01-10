bfd18702dee2060e438b8da359530fa6cef747da
German Sorokin <ger.sorokin@gmail.com>
2021-01-10 18:34:08 +0300

    main

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,41 @@
 let openButton = document.querySelector('.button__edit-info');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.button__popup_close');
+let saveButton = document.querySelector('.button__save');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
+let togglePopup = () => {
+  popup.classList.toggle('popup_opened')
+}
 
-openButton.addEventListener('click', () => {
-  popup.classList.add('popup_opened')
-})
+openButton.addEventListener('click', togglePopup)
 
-closeButton.addEventListener('click', () => {
-  popup.classList.remove('popup_opened')
-})
+closeButton.addEventListener('click', togglePopup)
 
 popup.addEventListener('click', (event) => {
-  if (event.target === popup) {
-    popup.classList.remove('popup_opened')
+  if (event.target === event.currentTarget) {
+    togglePopup()
   }
 
 })
+
+
+
+function saveProfile() {
+  let newProfileTitle = document.querySelector('.input__name');
+  let newProfileSubtitle = document.querySelector('.input__caption');
+
+  profileTitle.textContent = newProfileTitle.value;
+
+  profileSubtitle.textContent = newProfileSubtitle.value;
+
+  newProfileTitle.value = '';
+  newProfileSubtitle.value = '';
+
+}
+
+saveButton.addEventListener('click',saveProfile)
+saveButton.addEventListener('click', togglePopup)
+
+
