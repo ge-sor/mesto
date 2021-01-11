47701c292470e7886d3fba3435245c4ef81d4770
German Sorokin <ger.sorokin@gmail.com>
2021-01-12 00:01:51 +0300

    fix after 1 review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 let openButton = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-btn');
-let saveButton = document.querySelector('.popup__save-btn');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+
 
 let togglePopup = () => {
   popup.classList.toggle('popup_opened')
@@ -20,22 +20,43 @@ popup.addEventListener('click', (event) => {
 
 })
 
+togglePopup.addEventListener('click', function () {
+  let newProfileTitle = document.querySelector('.form__input_name');
+  let newProfileSubtitle = document.querySelector('.form__input_caption');
+
+  newProfileTitle.value = profileTitle;
+  newProfileSubTitle.value = profileSubtitle;
+
+});
+
+// Находим форму в DOM
+let formElement = querySelector('.form');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
+
+    // Находим поля формы в DOM
+    let newProfileTitle = document.querySelector('.form__input_name');
+    let newProfileSubtitle = document.querySelector('.form__input_caption');
+
+    // Получите значение полей из свойства value
+    newProfileTitle.value = profileTitle;
+    newProfileSubTitle.value = profileSubtitle;
 
 
-function saveProfile() {
-  let newProfileTitle = document.querySelector('.input__name');
-  let newProfileSubtitle = document.querySelector('.input__caption');
+    // Выберите элементы, куда должны быть вставлены значения полей
 
-  profileTitle.textContent = newProfileTitle.value;
-
-  profileSubtitle.textContent = newProfileSubtitle.value;
-
-  newProfileTitle.value = '';
-  newProfileSubtitle.value = '';
-
+    // Вставьте новые значения с помощью textContent
+    profileTitle.textContent = newProfileTitle.value;
+    profileSubtitle.textContent = newProfileSubtitle.value;
 }
 
-saveButton.addEventListener('click',saveProfile)
-saveButton.addEventListener('click', togglePopup)
-
+handleFormSubmit();
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
 
