63e767a515c503b5bec846826a1262f9a4579f77
German Sorokin <ger.sorokin@gmail.com>
2021-02-03 15:28:41 +0300

    validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,48 +1,76 @@
+// Функция, которая показывает ошибку
 const showInputError = (formElement, inputElement, errorMessage) => {
+  // Находим элемент ошибки внутри самой функции
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('form__input_type_error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('form__input-error_active');
 };
 
+// Функция, которая скрывает ошибку
 const hideInputError = (formElement, inputElement) => {
+  // Находим элемент ошибки
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('form__input_type_error');
   errorElement.classList.remove('form__input-error_active');
   errorElement.textContent = '';
 };
 
+/* Функция checkInputValidity принимает два параметра:
+formElement — html-элемент формы, в которой находится проверяемое поле ввода. 
+Он нужен для поиска элемента ошибки в форме.
+inputElement — проверяемое поле ввода. */
 const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
+    // showInputError получает параметром форму, в которой
+    // находится проверяемое поле, и само это поле
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
+    // hideInputError получает параметром форму, в которой
+    // находится проверяемое поле, и само это поле
     hideInputError(formElement, inputElement);
   }
 };
 
+
+// Функция принимает массив полей
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся фунцкция
+    // hasInvalidInput вернёт true
   return !inputElement.validity.valid;
 })
-  hasInvalidInput()
+hasInvalidInput()
 }
 
 
+
+// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
 const toggleButtonState = (inputList, buttonElement) => {
+  // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
   buttonElement.classList.add('button_inactive');
 } else {
+  // иначе сделай кнопку активной
   buttonElement.classList.remove('button_inactive');
 }
 }
 
 const setEventListeners = (formElement) => {
+  // Найдём все поля формы и сделаем из них массив
   const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  // Найдём в текущей форме кнопку отправки
   const buttonElement = formElement.querySelector('.form__submit');
+  // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
   toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
+
+      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
       toggleButtonState(inputList, buttonElement)
     });
   });
